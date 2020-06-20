d37b378de2f267feb3a98afff8cc3667a185ec2f
Kai <kaiann.fletcher@mail.utoronto.ca>
2020-06-20 10:56:53 -0400

    Completed Elearning Data.

--- covid19/model/elearningData.dart
+++ covid19/model/elearningData.dart
@@ -1,9 +1,3 @@
-// //step -1 import the http package post adding it dependency in pubspec.yml
-// import 'dart:convert';
-
-// import 'package:flutter/material.dart';
-// import 'package:http/http.dart' as http;
-
 class ElearningData {
   String category;
   String title;
@@ -32,15 +26,3 @@ class ElearningData {
     return data;
   }
 }
-
-// Future<ElearningData> fetchElearningData() async {
-//   final response = await http.get('http://api.springernature.com/metadata/pam?q=subject:Covid-19&api_key=3a7469fb6cb8df6570b75daf6ae3c550');
-//   print(json.decode(response.body).cast<Map<String, dynamic>>());
-//   debugPrint(response.body[1]);
-//   if (response.statusCode == 200) {
-//     return ElearningData.fromJson(json.decode(response.body[1]));
-//   }
-//   else {
-//     throw Exception('Failed to load Elearning data');
-//   }
-// }

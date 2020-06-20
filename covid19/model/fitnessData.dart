94194a53ea240f3c05dd3c92349ad1664cb88fb6
Kai <kaiann.fletcher@mail.utoronto.ca>
2020-06-20 09:45:46 -0400

    Fitness Complete.

---
+++ covid19/model/fitnessData.dart
@@ -0,0 +1,28 @@
+class FitnessData {
+  String category;
+  String title;
+  String summary;
+  String link;
+  String author;
+
+  FitnessData(
+      {this.category, this.title, this.summary, this.link, this.author});
+
+  FitnessData.fromJson(Map<String, dynamic> json) {
+    category = json['category'];
+    title = json['title'];
+    summary = json['summary'];
+    link = json['link'];
+    author = json['author'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['category'] = this.category;
+    data['title'] = this.title;
+    data['summary'] = this.summary;
+    data['link'] = this.link;
+    data['author'] = this.author;
+    return data;
+  }
+}
